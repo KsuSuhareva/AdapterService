b1d08021a97880e290db70baee98f7b89a2d0960
Kseniya Suhareva <suharevaksu86@gmail.com>
2023-02-28 11:38:28 +0300

    Initial commit

---
+++ src/main/java/by/suhareva/adapterservice/AdapterServiceApplication.java
@@ -0,0 +1,13 @@
+package by.suhareva.adapterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdapterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdapterServiceApplication.class, args);
+    }
+
+}
