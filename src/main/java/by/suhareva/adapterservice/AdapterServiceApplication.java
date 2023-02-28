14e2578c359b0f0c488538b9df900b47ae6983d8
Kseniya Suhareva <suharevaksu86@gmail.com>
2023-02-28 11:53:09 +0300

    создала ветку develop

--- src/main/java/by/suhareva/adapterservice/AdapterServiceApplication.java
+++ src/main/java/by/suhareva/adapterservice/AdapterServiceApplication.java
@@ -3,7 +3,7 @@ package by.suhareva.adapterservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages =  "by.suhareva.adapterservice")
 public class AdapterServiceApplication {
 
     public static void main(String[] args) {
