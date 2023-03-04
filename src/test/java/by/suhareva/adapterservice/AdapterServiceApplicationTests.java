e11639969ae226eb529e35e4dc8ba2d333956029
Kseniya Suhareva <suharevaksu86@gmail.com>
2023-03-04 21:11:55 +0300

    добавила интеграционное тестирование WebClient

--- src/test/java/by/suhareva/adapterservice/AdapterServiceApplicationTests.java
+++ src/test/java/by/suhareva/adapterservice/AdapterServiceApplicationTests.java
@@ -3,11 +3,8 @@ package by.suhareva.adapterservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class AdapterServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
