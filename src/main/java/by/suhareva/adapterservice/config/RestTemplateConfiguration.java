c1c044bec21f78d17ec4ca79641704434dfd1c03
Kseniya Suhareva <suharevaksu86@gmail.com>
2023-03-05 22:23:12 +0300

    добавила лишний импорт

--- src/main/java/by/suhareva/adapterservice/config/RestTemplateConfiguration.java
+++ src/main/java/by/suhareva/adapterservice/config/RestTemplateConfiguration.java
@@ -7,9 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.retry.backoff.FixedBackOffPolicy;
-import org.springframework.retry.policy.SimpleRetryPolicy;
-import org.springframework.retry.support.RetryTemplate;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriTemplateHandler;
 
