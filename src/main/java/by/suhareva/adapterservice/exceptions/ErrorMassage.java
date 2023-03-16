88296ace793ef1f47981181a2150078d7fff52d7
Kseniya Suhareva <suharevaksu86@gmail.com>
2023-03-16 10:55:53 +0300

    добавила описание схем в докуентацию, изменила статус

--- src/main/java/by/suhareva/adapterservice/exceptions/ErrorMassage.java
+++ src/main/java/by/suhareva/adapterservice/exceptions/ErrorMassage.java
@@ -18,7 +18,7 @@ public class ErrorMassage {
     @Schema(description = "The full date the error occurred", example = "2023-03-15 11:36:24.07")
     private Date date;
 
-    @Schema(description = "HTTP status", example = "404")
+    @Schema(description = "HTTP status", example = "400")
     private Integer status;
 
     @Schema(description = "ErrorMassage", example = "The Fine not found with number= 12AB123455")
